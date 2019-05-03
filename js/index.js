136821c295fb003c9d8f885c254a1bf981162556
Symon Hambrey <symonhambrey@gmail.com>
2019-05-03 13:13:58 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,49 +1,18 @@
-// https://www.npmjs.com/package/cordova-plugin-camera
+document.getElementById("btn").addEventListener
+   ("click", cameraTakePicture);
 
-var app = {
-    image: null,
-	imgOptions:null,
+   function cameraTakePicture() {
+   navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL
+   });
 
-    initialize: function() {
-      // Use deviceready on a device in in the emulator
-           //     document.addEventListener('deviceready', this.onDeviceReady, false);
-      // Use DOMContentLoaded in a browser
-        document.addEventListener("DOMContentLoaded", this.onDeviceReady, false);
-    },
+   function onSuccess(imageData) {
+      var image = document.getElementById('image');
+      image.src = "data:image/jpeg;base64," + imageData;
+   }
 
-    onDeviceReady: function() {
-        document.querySelector("#btn").addEventListener("click", app.callCamera);
-		console.log("button listener added");
-		app.image = document.querySelector("#image");
-    },
-
-    callCamera: function ( ) {
-		app.imgOptions = {quality : 75,
-				destinationType: Camera.DestinationType.DATA_URL,
-  				sourceType: Camera.PictureSourceType.CAMERA,
-				allowEdit : false,
-				encodingType : Camera.EncodingType.JPEG,
-				mediaType: Camera.MediaType.PICTURE,
-				targetWidth : 200,
-				cameraDirection : Camera.Direction.FRONT,
-				saveToPhotoAlbum : false
-			   };
-
-        navigator.camera.getPicture( app.imgSuccess, app.imgFail, app.imgOptions );
-    },
-
-	imgSuccess: function ( imageData ) {
-		//got an image back from the camera
-   		app.image.src = "data:image/jpeg;base64," + imageData;
-		console.log("Image loaded into interface");
-		//clear memory in app
-		navigator.camera.cleanup();
-	},
-
-	imgFail: function ( msg ) {
-		console.log("Failed to get image: " +  msg);
-	}
-
-};
-
-app.initialize();
+   function onFail(message) {
+      alert('Failed because: ' + message);
+   }
+}
